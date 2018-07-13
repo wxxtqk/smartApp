0e4fe46a132ec6ff4e81f3d7ccdfd7ee215729bf
LinAN12152408 <1240105800@qq.com>
2018-07-13 10:23:54 +0800

    study plan

--- pages/plan/plan.js
+++ pages/plan/plan.js
@@ -1,10 +1,11 @@
 //logs.js
 const util = require('../../utils/util.js');
 const plan_study_list = require('../../config/planConfig.js').plan_study_list;
+const SUCCESS_OK = "200";
 
 Page({
   data: {
-
+    planStudyList: []
   },
   // 事件处理
   goStudy: function(e){
@@ -16,9 +17,16 @@ Page({
     console.log(plan_study_list)
     wx.request({
       url: plan_study_list, //小程序目前发起request请求，必须是https协议
+      // 成功
       success:function(res) {
         console.log(res);
+        res = res.data;
+        if (res.state === SUCCESS_OK ) {
+          this.planStudyList = res.data.list;
+          console.log(this.planStudyList)
+        }
       },
+      // 失败
       fail:function(res) {
         console.log(res)
       }
