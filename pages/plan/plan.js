cac4b96c75982fafd1b26f9365faff15f4498ec8
tangquankun <1072317596@qq.com>
2018-07-29 13:54:31 +0800

    添加权限

--- pages/plan/plan.js
+++ pages/plan/plan.js
@@ -1,5 +1,7 @@
 //plan.js
-import { plan_study_list } from '../../api/planConfig.js'
+import {
+  plan_study_list
+} from '../../api/planConfig.js'
 const SUCCESS_OK = "200";
 const app = getApp()
 Page({
@@ -7,7 +9,7 @@ Page({
     planStudyList: []
   },
   // 事件处理
-  goStudy: function(e){
+  goStudy: function (e) {
     console.log(e)
   },
   // 跳转到其他页面
@@ -17,11 +19,11 @@ Page({
     let studyDetails = event.currentTarget.dataset.details
     // 页面跳转传值
     wx.navigateTo({
-      url: url +'?details='+ JSON.stringify(studyDetails)
+      url: url + '?details=' + JSON.stringify(studyDetails)
     })
   },
   // 请求学习课程列表的接口
-  gitPlanStudy: function() {
+  gitPlanStudy: function () {
     wx.showLoading({
       title: '加载中',
       mask: true
@@ -43,15 +45,22 @@ Page({
         title: '提示',
         content: '链接数据库失败'
       })
-    }) 
+    })
   },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function() {
+  onLoad: function () {
     var that = this;
-    that.gitPlanStudy();
-    console.log(app)
+    // 用户登录后才可以跳转查看
+    if (app.globalData.userInfo) {
+      that.gitPlanStudy()
+    } else {
+      wx.showModal({
+        title: '提示',
+        content: '请先登录'
+      })
+    }
   },
 
   /**
