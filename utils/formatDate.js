a75672e976892fcf0a32fdb069e2a8ce86fd1706
LinAN12152408 <1240105800@qq.com>
2018-08-03 14:35:25 +0800

    plan

--- utils/formatDate.js
+++ utils/formatDate.js
@@ -1,45 +1,11 @@
-// const formatTime = date => {
-//   const year = date.getFullYear()
-//   const month = date.getMonth() + 1
-//   const day = date.getDate()
-//   const hour = date.getHours()
-//   const minute = date.getMinutes()
-//   const second = date.getSeconds()
-
-//   return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
-// }
-// const formatDate = date => {
-//   const year = date.getFullYear()
-//   const month = date.getMonth() + 1
-//   const day = date.getDate()
-
-//   return [year, month, day].map(formatNumber).join('-')
-// }
-// const formatNumber = n => {
-//   n = n.toString()
-//   return n[1] ? n : '0' + n
-// }
-// const formatTimeNew = date => {
-//   const year = date.getFullYear()
-//   const month = date.getMonth() + 1
-//   const day = date.getDate()
-//   const hour = date.getHours()
-//   const minute = date.getMinutes()
-//   const second = date.getSeconds()
-//   return [hour, minute, second].map(formatNumber).join(':')
-// }
-// module.exports = {
-//   formatTime: formatTime,
-//   formatDate: formatDate,
-//   formatTimeNew: formatTimeNew
-// }
+// 时间转换 秒转为分秒
 
 export function formatTimeNew(time){
   if(typeof time !== 'number' || time < 0){
     return time
   }
-  var hour = parseInt(time/ 3600)
-  time = time % 3600
+  // var hour = parseInt(time/ 3600)
+  // time = time % 3600
   var minte = parseInt(time / 60)
   time = parseInt( time % 60)
   var second = time
